410605da7a34bd50ed3738a5ada5dbee51c23231
sch <sch102403@gmail.com>
2023-02-06 19:19:59 +0900

    backup prev nvim setting

---
+++ nvim.back/lua/sch/plugins/telescope.lua
@@ -0,0 +1,41 @@
+-- import telescope plugin safely
+local telescope_setup, telescope = pcall(require, "telescope")
+if not telescope_setup then
+  return
+end
+
+-- import telescope actions safely
+local actions_setup, actions = pcall(require, "telescope.actions")
+if not actions_setup then
+  return
+end
+
+-- configure telescope
+telescope.setup({
+  -- configure custom mappings
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+        ["<C-j>"] = actions.move_selection_next, -- move to next result
+        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+      },
+    },
+  },
+  extensions = {
+    repo = {
+      list = {
+        fd_opts = {
+          "--no-ignore-vcs",
+        },
+        search_dirs = {
+          "~/projects/",
+        },
+      },
+    },
+  },
+})
+
+telescope.load_extension("fzf")
+telescope.load_extension("project")
+telescope.load_extension("repo")
