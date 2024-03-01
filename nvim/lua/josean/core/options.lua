cf97506b339483b3373153bcf19ddaa887c120ed
chaneesong <sch102403@gmail.com>
2024-03-01 12:58:26 +0900

    nvim 설정 변경

--- nvim/lua/josean/core/options.lua
+++ nvim/lua/josean/core/options.lua
@@ -38,7 +38,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-opt.iskeyword:append("-") -- consider string-string as whole word
-
--- ruler
-opt.colorcolumn = "80"
+-- turn off swapfile
+opt.swapfile = false
